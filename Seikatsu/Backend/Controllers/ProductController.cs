0e82abf4934c0a62e61e91d67c047ef817413802
Yashas Hegde <126957167+Hikigaya69@users.noreply.github.com>
2026-02-28 18:38:00 +0530

    random products for index page is almost ready

--- Seikatsu/Backend/Controllers/ProductController.cs
+++ Seikatsu/Backend/Controllers/ProductController.cs
@@ -21,6 +21,11 @@ namespace Seikatsu.Backend.Controllers
     public class ProductController (IProductService productService) : ControllerBase
     {
         [HttpGet("productforindex")]
+        // This endpoint retrieves a list of random products for the index page. The client can specify the number of products
+        // to retrieve using the 'count' query parameter, which defaults to 10 if not provided. The response is wrapped in an
+        // APIResponse object that indicates success and includes the data or an appropriate message if no products are found.
+        //https://localhost:7115/api/Product/GetRandomProducts this is the endpoint which the frontend will call to get the random products for the index page.     
+
         public async Task<ActionResult<IEnumerable<ProductDTOforIndexPage>>> GetRandomProducts(int count = 10)
         {
             var products = await productService.GetRandomProductsAsync(count);
