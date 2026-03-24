3e7e32b1a326fadceb9da3e69749900e1dccf170
ashikajnair <ashikajn7@gmail.com>
2026-03-24 09:03:19 +0530

    deliverables updated

--- Seikatsu/Frontend/src/components/home/HomeNavbar.jsx
+++ Seikatsu/Frontend/src/components/home/HomeNavbar.jsx
@@ -1,10 +1,35 @@
 ﻿import { ShoppingCart, User,LogOut } from "lucide-react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function HomeNavbar() {
   const navigate = useNavigate();
+const [setCartCount] = useState(0);
+useEffect(() => {
 
+  const fetchCartSummary = async () => {
+
+    try {
+
+      const res = await axios.get(
+        "/api/Cart/cartsummary",
+        { withCredentials: true }
+      );
+
+      setCartCount(res.data.data.totalItems);
+
+    } catch (err) {
+
+      console.error(err);
+
+    }
+
+  };
+
+  fetchCartSummary();
+
+}, []);
 const handleLogout = async () => {
 
     try {
@@ -44,7 +69,10 @@ const handleLogout = async () => {
 
           <User className="cursor-pointer" />
 
-          <ShoppingCart className="cursor-pointer" />
+          <ShoppingCart
+  className="cursor-pointer"
+  onClick={() => navigate("/cart")}
+/>
           <button
   onClick={handleLogout}
   className="flex items-center gap-2 border border-gray-300 px-3 py-1 rounded-lg shadow-sm hover:bg-gray-100 transition"
